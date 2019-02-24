ca267e9e1c8892bd68b14d400df6b15b8fe306a6
alexomaset <alexomaset711@gmail.com>
2019-02-24 14:46:23 +0300

    added a second function to js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,30 @@ var q2 = parseInt($("input:radio[name=2]:checked").val());
 var q3 = parseInt($("input:radio[name=3]:checked").val());
 var q4 = parseInt($("input:radio[name=4]:checked").val());
 var q5 = parseInt($("input:radio[name=5]:checked").val());
-$("#display").text(q1+q2+q3+q4+q5);
-});
+
+//empty list
+var sum1 = q1 + q2 + q3 + q4 + q5
+var result = '';
+
+function getResult(sum) {
+  if (sum < 50) {
+    result = 'You have failed please retake the test!';
+    return result
+  } else if (50 < sum && sum < 80) {
+    result = 'you have fairly passed!';
+    return result
+  } else {
+    result = 'congratualtions you have passed the test!';
+    return result
+  }
+}
+
+displayResult = getResult(sum1);
+
+$("#display").text(displayResult);
+  });
+
+$('#restart').click(function() {
+    location.reload();
+  });
 });
